ba5508eb13dbec74b4d6369a05c6ab9113fc3d00
Duc Nguyen <duogduc98@gmail.com>
2018-06-04 18:29:58 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 console.log('testing')
+const button1 = document.querySelector(".but1")
+const button2 = document.querySelector(".but2")
+const inputField = document.querySelector("input")
+const submit1 = document.querySelector("#submit")
+
+function sayYeah1() {
+    let heading = document.querySelector('h1')
+    heading.textContent = "Changed the first headline"
+  }
+
+function sayYeah2() {
+    let heading = document.querySelectorAll('h1')
+    heading[1].textContent = "Changed the second headline"
+  }
+
+function myFunction(){
+    let heading2 = document.querySelectorAll('h1')
+    let x = document.getElementById("frm1")[0].value;
+    //let x = document.getElementsByTagName("input")[0].value;
+    heading2[2].textContent = x
+}
+
+
+function submit(){
+    let heading2 = document.querySelectorAll('h1')
+    let x = document.getElementById("text").value;
+    heading2[2].textContent = x
+}
+
+
+button1.addEventListener('click', sayYeah1)
+button2.addEventListener('click', sayYeah2)
+submit1.addEventListener('click',submit)
+
+inputField.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        submit1.click();
+    // Trigger the button element with a click
+    }
+    
+    });
+
+
+
